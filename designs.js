656ba82188735a5211fb3e826cdb907d474ef7e0
JSONRete <jsonrete@gmail.com>
2022-07-31 23:59:21 -0400

    working on pixel_art_2

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
 // Select color input
+const colorSelect = document.getElementById('colorPicker');
 // Select size input
+const gridSize = document.getElementById('sizePicker');
+const height = document.getElementById('inputHeight');
+const width = document.getElementById('inputWidth');
+
+const canvasGrid = document.getElementById('pixelCanvas');
 
 // When size is submitted by the user, call makeGrid()
+const pickSize = function (event) {
+    event.preventDefault();
+    makeGrid();
+    console.log("test")
+};
 
 function makeGrid() {
-
+    // const makeGrid = function() {
 // Your code goes here!
+canvasGrid.innerHTML = ""
+for (let row = 0; row < height.value; row++){
+    const tr = document.createElement('tr')
+    for (let cell = 0; cell < width.value; cell++){
+    const td = document.createElement('td')
+    td.addEventListener('click', colorSelect)
+    tr.append(td)
+    }
+    canvasGrid.append(tr)
 
+    }
 }
+
+
+
+
+
+
+canvasGrid
